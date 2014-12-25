4b0686be402e74463cf7f6bf07f2dbb36d0b3ff8
Jonas Wisplinghoff <mail@jonaswisplinghoff.de>
2014-12-25 09:54:25 +0100

    Initial Commit

---
+++ BLEHeartRate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BLEHeartRate
+//
+//  Created by Jonas Wisplinghoff on 25.12.14.
+//  Copyright (c) 2014 Jonas Wisplinghoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
